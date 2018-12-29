c7ff7725ef58e225d2b7f36afc8f99dc4852a052
Sid <ssharma1081@gmail.com>
2018-12-29 06:21:52 +0000

    Setup express with a / endpoint

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const port = '3000'
+
+app.get('/', (req, res) => {
+    res.send('Hello Flash!')
+})
+
+app.listen(port, _ => {
+    console.log(`Climbing API is running on port ${port}`)
+})
