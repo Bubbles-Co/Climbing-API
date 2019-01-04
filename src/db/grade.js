f96766139cd8285605c5b44ccfc8925a8eb9c5ec
Siddharth Sharma <ssharma1081@gmail.com>
2019-01-04 16:48:49 +0530

    wip

--- src/db/grade.js
+++ src/db/grade.js
@@ -1,8 +1,20 @@
-const knex = require('knex')
+const knex = require('knex')({
+    client: 'pg',
+    connection: 'postgres://flashadmin:FlashAdmin123@localhost:5432/flash',
+    migrations: {
+        tableName: 'knex_migrations'
+    },
+    seeds: {
+        directory: './seeds/dev'
+    }
+})
 
 const fetchGrades = () => {
-    return knex('grade')
-        .select('*')
+    console.log(knex)
+    return knex
+
+        // .select('id')
+        // .from('grade')
         .then(console.log)
         .catch(err => {
             console.log('Error while fetching grades: ', err)
